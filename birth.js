2a7619072748f0d16e1a653c385edc2e404aec0a
Micke-dev <mickeiuzo.ms@gmail.com>
2024-10-01 17:28:41 -0300

    Add files via upload

---
+++ birth.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-birth');
+const valor_A = document.getElementById('a'); 
+const valor_B = document.getElementById('b'); 
+
+function validaCampo (campoA, campoB) { 
+    return campoB > campoA; 
+};
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault(); 
+    const valor_a = parseFloat(valor_A.value);
+    const valor_b = parseFloat(valor_B.value);
+
+    const mensagemSucesso = `O valor é válido porque: <b>${b.value}</b> é maior que <b>${a.value}</b>.<br>`;
+    
+        
+    formEValido = validaCampo(a, b);
+    
+    if (formEValido)    {
+        
+        const containerMensagem = document.querySelector('.mensagem-sucesso'); 
+        containerMensagem.innerHTML = mensagemSucesso; 
+        containerMensagem.style.display = 'block'; 
+        document.querySelector('.mensagem-erro').style.display = 'none';
+    
+        }
+
+        
+        else {
+
+        dodocument.querySelector('.mensagem-erro').style.display = 'block';
+        document.querySelector('.mensagem-sucesso').style.display = 'none';
+        
+    } 
+
+}
+
+)
